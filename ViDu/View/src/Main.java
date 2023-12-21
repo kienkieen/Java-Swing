bc1263aa101af4b0011757b67c2f68bcbe6aa6de
kienkieen <trungkiena1206052004@gmail.com>
2023-12-21 10:44:02 +0700

    Merge remote-tracking branch 'origin/main'

--- ViDu/View/src/Main.java
+++ ViDu/View/src/Main.java
@@ -2,6 +2,23 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame jf = new JFrame();
+        // Gan ten
+        jf.setTitle("Vi Du ve JFrame");
+        // Gan kich co
+        jf.setSize(600, 400);
+
+//        while ((true)) {
+//            System.out.println("Chuong trinh dang chay!");
+//        }
+
+        // Gan vi tri hien thi
+        jf.setLocation(300, 300);
+
+        // Thoat chuong trinh
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        // Cho phep hien thi
         jf.setVisible(true);
     }
 }
